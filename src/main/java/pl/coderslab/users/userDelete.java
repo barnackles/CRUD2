452af259e52d44eb3cddd7740e50f9668d3603c9
barnackles <p.martewicz@yahoo.com>
2022-04-19 21:03:31 +0200

    delete v 1.0 (without warning)

--- src/main/java/pl/coderslab/users/userDelete.java
+++ src/main/java/pl/coderslab/users/userDelete.java
@@ -2,30 +2,28 @@ package pl.coderslab.users;
 
 import pl.coderslab.entity.UserDao;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 
 @WebServlet(name = "userDelete", value = "/user/delete")
 public class userDelete extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
+            int userId = Integer.parseInt(request.getParameter("id"));
+            UserDao userToDeleteDao = new UserDao();
+            userToDeleteDao.delete(userId);
+            response.sendRedirect("/user/list");
 
-        int userId = Integer.parseInt(request.getParameter("id"));
-        UserDao userToDeleteDao = new UserDao();
-        userToDeleteDao.delete(userId);
 
     }
 }
